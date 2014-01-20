1df384d63e5a84a1723a2dcaa4932df8cc485a35
park sung chan <dnrmrgks@naver.com>
2014-01-20 11:08:55 +0900

    Initial Commit

---
+++ Ex3-2.1.1Tests/Ex3_2_1_1Tests.m
@@ -0,0 +1,34 @@
+//
+//  Ex3_2_1_1Tests.m
+//  Ex3-2.1.1Tests
+//
+//  Created by SDT-1 on 2014. 1. 20..
+//  Copyright (c) 2014년 T. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Ex3_2_1_1Tests : XCTestCase
+
+@end
+
+@implementation Ex3_2_1_1Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
